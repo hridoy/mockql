cc0fff1a54bb12bfca99d188c1578d28a2fced27
Hridoy <ac@hridoy.net>
2021-04-06 17:09:12 +0530

    generate base project

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,106 @@
-import 'reflect-metadata';
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import { buildSchema } from 'type-graphql';
-import ProductResolver from './services/product/product.resolver';
-import IntegrationResolver from './services/integration/integration.resolver';
-import ProfileResolver from './services/profile/profile.resolver';
-import DashboardResolver from './services/dashboard/dashboard.resolver';
-import InvoiceResolver from './services/invoice/invoice.resolver';
+import * as admin from 'firebase-admin';
+import { ApolloServer, ApolloError, ValidationError, gql } from 'apollo-server';
+const serviceAccount = require('../service-account.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
-(async () => {
-  const app = express();
+interface User {
+  id: string;
+  name: string;
+  screenName: string;
+  statusesCount: number;
+}
 
-  const apolloServer = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [
-        ProductResolver,
-        IntegrationResolver,
-        ProfileResolver,
-        DashboardResolver,
-        InvoiceResolver,
-      ],
-      validate: false,
-    }),
-    introspection: true, // enables introspection of the schema
-    playground: true, // enables the actual playground
-    // context: ({ req, res }) => ({ req, res }),
-  });
+interface Tweet {
+  id: string;
+  likes: number;
+  text: string;
+  userId: string;
+}
 
-  const port = process.env.PORT || 4000;
-  const path = '/';
+const typeDefs = gql`
+  # A Twitter User
+  type User {
+    id: ID!
+    name: String!
+    screenName: String!
+    statusesCount: Int!
+    tweets: [Tweets]!
+  }
 
-  apolloServer.applyMiddleware({ app, path, cors: true });
-  app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}/`);
-  });
-})();
+  # A Tweet Object
+  type Tweets {
+    id: ID!
+    text: String!
+    userId: String!
+    user: User!
+    likes: Int!
+  }
+
+  type Query {
+    tweets: [Tweets]
+    user(id: String!): User
+  }
+`;
+
+const resolvers = {
+  Query: {
+    async tweets() {
+      const tweets = await admin
+        .firestore()
+        .collection('tweets')
+        .get();
+      return tweets.docs.map(tweet => tweet.data()) as Tweet[];
+    },
+    async user(_: null, args: { id: string }) {
+      try {
+        const userDoc = await admin
+          .firestore()
+          .doc(`users/${args.id}`)
+          .get();
+        const user = userDoc.data() as User | undefined;
+        return user || new ValidationError('User ID not found');
+      } catch (error) {
+        throw new ApolloError(error);
+      }
+    }
+  },
+  User: {
+    async tweets(user) {
+      try {
+        const userTweets = await admin
+          .firestore()
+          .collection('tweets')
+          .where('userId', '==', user.id)
+          .get();
+        return userTweets.docs.map(tweet => tweet.data()) as Tweet[];
+      } catch (error) {
+        throw new ApolloError(error);
+      }
+    }
+  },
+  Tweets: {
+    async user(tweet) {
+      try {
+        const tweetAuthor = await admin
+          .firestore()
+          .doc(`users/${tweet.userId}`)
+          .get();
+        return tweetAuthor.data() as User;
+      } catch (error) {
+        throw new ApolloError(error);
+      }
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true
+});
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
